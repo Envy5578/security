5d12ff50cd782b020bc3659b956aeeee261ff7fa
Radmir Musabaev <musabaevradmir@gmail.com>
2024-03-25 17:56:39 +0400

    feat: added allowed paths to config

--- src/main/java/com/focus_group/security/configs/SecurityConfiguration.java
+++ src/main/java/com/focus_group/security/configs/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.focus_group.security.configs;
 
 import java.util.List;
 
+import com.focus_group.security.properties.ApplicationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -25,39 +26,39 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity
 public class SecurityConfiguration {
     private static final String[] ALLOWED_PATHS = {
-        "/api/v1/auth/**",
-        "/v2/api-docs",
-        "/actuator/**",
-        "/error",
-        "/v3/api-docs",
-        "/v3/api-docs/**",
-        "/swagger-resources",
-        "/swagger-resources/**",
-        "/configuration/ui",
-        "/configuration/security",
-        "/swagger-ui/**",
-        "/webjars/**",
-        "/swagger-ui.html",
-        "/ws/info",
-        "/ws/**",
-        "/ws",
-        "/"};
-        // private final Web web;
+            "/api/v1/auth/**",
+            "/v2/api-docs",
+            "/actuator/**",
+            "/error",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html",
+            "/ws/info",
+            "/ws/**",
+            "/ws",
+            "/"};
 
-        private final AuthenticationProvider authenticationProvider;
-        private final UserNotEnabledExceptionHandler userNotEnabledExceptionHandler;
-        private final JwtAuthenticationFilter jwtAuthFilter;
-        
+    private final ApplicationProperties applicationProperties;
+    private final AuthenticationProvider authenticationProvider;
+    private final UserNotEnabledExceptionHandler userNotEnabledExceptionHandler;
+    private final JwtAuthenticationFilter jwtAuthFilter;
 
-        @Bean
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        
+
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration corsConfiguration = new CorsConfiguration();
                     corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-                    // corsConfiguration.setAllowedOriginPatterns(web.getCors().getAllowedOrigins()); // TODO: fix CORS
+                    corsConfiguration.setAllowedOriginPatterns(applicationProperties.getAllowedOrigins()); // TODO: fix CORS
                     corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
                     corsConfiguration.setAllowCredentials(true);
                     corsConfiguration.setExposedHeaders(List.of("Authorization"));
