ba98a20b312d398c0dff8ceb638e236728e08247
Radmir Musabaev <musabaevradmir@gmail.com>
2024-03-26 16:09:11 +0400

    feat: added bearer prefix

--- src/main/java/com/focus_group/security/services/LogoutService.java
+++ src/main/java/com/focus_group/security/services/LogoutService.java
@@ -1,18 +1,16 @@
 package com.focus_group.security.services;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 @Service
 public class LogoutService implements LogoutHandler {
-    //TODO : add BEARER_PREFIX
-    private static final String BEARER_PREFIX = null;
+    private static final String BEARER_PREFIX = "Bearer ";
 
     @Override
     public void logout(
@@ -26,5 +24,5 @@ public class LogoutService implements LogoutHandler {
         }
         SecurityContextHolder.clearContext();
     }
-    
+
 }
