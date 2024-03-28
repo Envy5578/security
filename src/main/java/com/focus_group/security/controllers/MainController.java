b79b8ae0cd4131bea61c0a9867a26f316a71af4a
Radmir Musabaev <musabaevradmir@gmail.com>
2024-03-28 18:57:22 +0400

    feat: created get me endpoint

--- src/main/java/com/focus_group/security/controllers/MainController.java
+++ src/main/java/com/focus_group/security/controllers/MainController.java
@@ -1,5 +1,8 @@
 package com.focus_group.security.controllers;
 
+import com.focus_group.security.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +12,13 @@ import java.security.Principal;
 
 @RequestMapping("/secured")
 @RestController
+@RequiredArgsConstructor
 public class MainController {
+    private final UserService service;
+
     @GetMapping("/user")
-    public String getUser(Principal principal) {
-        return principal.getName();
+    public ResponseEntity<?> getUser() {
+        return ResponseEntity.ok(service.getMe());
     }
 
 }
