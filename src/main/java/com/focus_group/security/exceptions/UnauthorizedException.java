6d3e1e606ef983a122a2d83846efd5b0b28748cd
Radmir Musabaev <musabaevradmir@gmail.com>
2024-04-08 15:19:13 +0400

    feat: created exceptions

--- src/main/java/com/focus_group/security/exceptions/UnauthorizedException.java
+++ src/main/java/com/focus_group/security/exceptions/UnauthorizedException.java
@@ -1,18 +1,16 @@
 package com.focus_group.security.exceptions;
 
 import com.focus_group.security.enumType.ErrorCode;
+import lombok.Getter;
 import org.springframework.security.core.AuthenticationException;
 
+@Getter
 public class UnauthorizedException extends AuthenticationException {
 
     private ErrorCode errorCode;
 
     public UnauthorizedException(ErrorCode errorType) {
         super(errorType.getMessage());
-    }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
     }
 
 
--- src/main/java/com/focus_group/security/exceptions/UserNotEnabledExceptionHandler.java
+++ src/main/java/com/focus_group/security/exceptions/UserNotEnabledExceptionHandler.java
@@ -1,15 +1,20 @@
 package com.focus_group.security.exceptions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.focus_group.security.dto.ErrorDTO;
+import com.focus_group.security.enumType.ErrorCode;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Component
@@ -22,17 +27,17 @@ public class UserNotEnabledExceptionHandler implements AuthenticationEntryPoint 
                          AuthenticationException authException) throws IOException, ServletException {
         // log.error(e);
 
-        // ErrorDTO errorDTO = ErrorDTO.builder()
-        //         .message(ErrorCode.EMAIL_NOT_VERIFIED.getMessage())
-        //         .errorCode(ErrorCode.EMAIL_NOT_VERIFIED.name())
-        //         .error(HttpStatus.UNAUTHORIZED.getReasonPhrase())
-        //         .statusCode(HttpStatus.UNAUTHORIZED.value())
-        //         .path(request.getRequestURI())
-        //         .timestamp(LocalDateTime.now())
-        //         .build();
-        // response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        // response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        // response.getWriter().write(mapper.writeValueAsString(errorDTO));
+        ErrorDTO errorDTO = ErrorDTO.builder()
+                .message(ErrorCode.EMAIL_NOT_VERIFIED.getMessage())
+                .errorCode(ErrorCode.EMAIL_NOT_VERIFIED.name())
+                .error(HttpStatus.UNAUTHORIZED.getReasonPhrase())
+                .statusCode(HttpStatus.UNAUTHORIZED.value())
+                .path(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .build();
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(mapper.writeValueAsString(errorDTO));
     }
 
 }
