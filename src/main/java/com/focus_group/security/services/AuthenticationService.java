99a105a7bce1246ec8d447cf578404920d645bae
Radmir Musabaev <musabaevradmir@gmail.com>
2024-04-08 15:24:04 +0400

    refactor: added invalid credential exception

--- src/main/java/com/focus_group/security/services/AuthenticationService.java
+++ src/main/java/com/focus_group/security/services/AuthenticationService.java
@@ -1,5 +1,15 @@
 package com.focus_group.security.services;
 
+import com.focus_group.security.dto.AuthenticationRequest;
+import com.focus_group.security.dto.AuthenticationResponse;
+import com.focus_group.security.dto.RegistrationRequest;
+import com.focus_group.security.enumType.ErrorCode;
+import com.focus_group.security.enumType.TokenType;
+import com.focus_group.security.exceptions.BadRequestException;
+import com.focus_group.security.tokens.JwtTokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,16 +17,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.focus_group.security.dto.AuthenticationRequest;
-import com.focus_group.security.dto.AuthenticationResponse;
-import com.focus_group.security.dto.RegistrationRequest;
-import com.focus_group.security.enumType.TokenType;
-import com.focus_group.security.tokens.JwtTokenService;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 
 
 @Service
@@ -27,29 +27,27 @@ public class AuthenticationService {
     private final UserService userService;
     private final JwtTokenService jwtTokenService;
     private final AuthenticationManager authenticationManager;
-    
+
     public void register(RegistrationRequest register) {
         userService.save(register);
         // sendMailVerification(register);
     }
-
 
     private void sendMailVerification(RegistrationRequest register) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'sendMailVerification'");
     }
 
-
     public AuthenticationResponse signIn(@Valid AuthenticationRequest register, HttpServletRequest request) {
         final Authentication authentication;
-        try{
+        try {
             authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(register.email(), register.password()));
-            }catch (BadCredentialsException e) {
-            throw new RuntimeException("Invalid username or password");
+        } catch (BadCredentialsException e) {
+            throw new BadRequestException(ErrorCode.INVALID_CREDENTIALS);
         }
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String accessToken = jwtTokenService.generateAccessToken(register.email());
         String refreshToken = jwtTokenService.generateRefreshToken(register.email(), TokenType.REFRESH_TOKEN);
-        return new AuthenticationResponse(TokenType.REFRESH_TOKEN.name() , accessToken, ACCESS_TOKEN_EXPIRES_IN_MINUTES, refreshToken);
+        return new AuthenticationResponse(TokenType.REFRESH_TOKEN.name(), accessToken, ACCESS_TOKEN_EXPIRES_IN_MINUTES, refreshToken);
     }
 }
