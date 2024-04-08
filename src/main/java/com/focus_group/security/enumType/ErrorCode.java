914f8e070f21ac84755b764350a2e541a244d002
Radmir Musabaev <musabaevradmir@gmail.com>
2024-04-08 15:20:39 +0400

    feat: added new errors and status code for errors

--- src/main/java/com/focus_group/security/enumType/ErrorCode.java
+++ src/main/java/com/focus_group/security/enumType/ErrorCode.java
@@ -1,18 +1,23 @@
 package com.focus_group.security.enumType;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
 public enum ErrorCode {
-    UNAUTHORIZED_ACCESS("Unauthorized access"),
-    INVALID_CREDENTIALS("Invalid credentials"),
-    EXPIRED_TOKEN("Expired token"),
-    INVALID_ACCESS_TOKEN("Invalid access token");
-
+    UNAUTHORIZED_ACCESS("Unauthorized access", HttpStatusCode.valueOf(401)),
+    INVALID_CREDENTIALS("Invalid credentials", HttpStatusCode.valueOf(401)),
+    EXPIRED_TOKEN("Expired token", HttpStatusCode.valueOf(401)),
+    INVALID_ACCESS_TOKEN("Invalid access token", HttpStatusCode.valueOf(401)),
+    EMAIL_NOT_VERIFIED("Email not verified", HttpStatusCode.valueOf(403)),
+    EMAIL_ALREADY_REGISTERED("Email already registered", HttpStatusCode.valueOf(403));
+    @Getter
     private final String message;
+    private final HttpStatusCode statusCode;
 
-    ErrorCode(String message) {
+    ErrorCode(String message, HttpStatusCode statusCode) {
         this.message = message;
+        this.statusCode = statusCode;
     }
 
-    public String getMessage() {
-        return message;
-    }
 }
