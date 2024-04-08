1728093e92a98458a051e6bad0f62cba8fda08ce
Radmir Musabaev <musabaevradmir@gmail.com>
2024-04-08 15:22:13 +0400

    feat: added error handler

--- src/main/java/com/focus_group/security/services/UserService.java
+++ src/main/java/com/focus_group/security/services/UserService.java
@@ -1,20 +1,20 @@
 package com.focus_group.security.services;
 
-import java.util.Optional;
-
+import com.focus_group.security.dto.RegistrationRequest;
+import com.focus_group.security.dto.UserDTO;
+import com.focus_group.security.entities.UserEntity;
+import com.focus_group.security.enumType.ErrorCode;
+import com.focus_group.security.exceptions.BadRequestException;
+import com.focus_group.security.mapper.UserMapper;
+import com.focus_group.security.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.focus_group.security.dto.RegistrationRequest;
-import com.focus_group.security.dto.UserDTO;
-import com.focus_group.security.entities.UserEntity;
-import com.focus_group.security.mapper.UserMapper;
-import com.focus_group.security.repositories.UserRepository;
-
-import lombok.RequiredArgsConstructor;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,12 +22,20 @@ public class UserService {
 
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
+
     public Optional<UserEntity> findByEmail(String email) {
         return repository.findByEmail(email);
     }
 
     @SuppressWarnings("null")
     public void save(RegistrationRequest register) {
+        Optional<UserEntity> existingUser = repository.findByEmail(register.email());
+        if (existingUser.isPresent()) {
+            throw new BadRequestException(
+                    ErrorCode.EMAIL_ALREADY_REGISTERED
+            );
+        }
+
         UserEntity user = UserEntity.builder()
                 .firstName(register.firstName())
                 .lastName(register.lastName())
